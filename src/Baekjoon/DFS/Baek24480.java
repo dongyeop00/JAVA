90b1cc1db6be3fd2881c348ed04f68f66ec81789
dongyeop00 <ehd_duq94@naver.com>
2024-12-13 13:21:26 +0900

    24.12.13 : 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ src/Baekjoon/DFS/Baek24480.java
@@ -0,0 +1,81 @@
+package Baekjoon.DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/* 알고리즘 수업 - 깊이 우선 탐색 2
+오늘도 서준이는 깊이 우선 탐색(DFS) 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+
+N개의 정점과 M개의 간선으로 구성된 무방향 그래프(undirected graph)가 주어진다.
+정점 번호는 1번부터 N번이고 모든 간선의 가중치는 1이다. 정점 R에서 시작하여 깊이 우선 탐색으로 노드를 방문할 경우 노드의 방문 순서를 출력하자.
+
+깊이 우선 탐색 의사 코드는 다음과 같다. 인접 정점은 내림차순으로 방문한다.
+
+==========================================================================================
+dfs(V, E, R) {  # V : 정점 집합, E : 간선 집합, R : 시작 정점
+    visited[R] <- YES;  # 시작 정점 R을 방문 했다고 표시한다.
+    for each x ∈ E(R)  # E(R) : 정점 R의 인접 정점 집합.(정점 번호를 내림차순으로 방문한다)
+        if (visited[x] = NO) then dfs(V, E, x);
+}
+==========================================================================================
+ */
+
+public class Baek24480 {
+
+    static int count;
+    static int[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int N, M, R;
+
+    public static void main(String[] arg) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        // 그래프 초기화
+        for(int i=0; i<N+1; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        visited = new int[N+1];
+        count = 0;
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+
+        for(int i=0; i<N+1; i++){
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        DFS(R);
+
+        for(int i=1; i<N+1; i++){
+            System.out.println(visited[i]);
+        }
+    }
+
+    private static void DFS(int v){
+        count++;
+        visited[v] = count;
+
+        for(int i=0; i<graph.get(v).size(); i++){
+            int node = graph.get(v).get(i);
+            if(visited[node] == 0){
+                DFS(node);
+            }
+        }
+    }
+}
