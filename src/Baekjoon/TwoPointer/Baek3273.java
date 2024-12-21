6d505697aae0860998bfe36bf71074ee2aa02cbf
dongyeop00 <ehd_duq94@naver.com>
2024-12-21 23:51:48 +0900

    24.12.21 : 두 수의 합

---
+++ src/Baekjoon/TwoPointer/Baek3273.java
@@ -0,0 +1,48 @@
+package Baekjoon.TwoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baek3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        int[] arr = new int[N];
+        int count = 0;
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int x = Integer.parseInt(bufferedReader.readLine());
+
+        int start = 0;
+        int end = N-1;
+
+        while(start < end){
+            int sum = arr[start] + arr[end];
+            if(sum == x){
+                count++;
+                start++;
+                end--;
+            }else if(sum > x){
+                end--;
+            }else{
+                start++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
