b083956f3eb02c1ae3cffffea3735ab5723c751b
구동엽 <ehd_duq94@naver.com>
2025-01-20 10:04:21 +0900

    25.01.20 : 쉽게 푸는 문제

---
+++ src/Baekjoon/Simulation/Baek1292.java
@@ -0,0 +1,30 @@
+package Baekjoon.Simulation;
+
+import java.util.Scanner;
+
+public class Baek1292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int[] arr = new int[1001];
+        int tot=0, count=0;
+        for(int i=1; i<B+1; i++){
+            for(int j=1; j<=i; j++){
+                if(count >= arr.length-1)
+                    break;
+                arr[count]=i;
+                count++;
+
+            }
+        }
+
+        for(int j=A-1; j<=B-1; j++){
+            tot += arr[j];
+        }
+
+        System.out.println(tot);
+    }
+}
