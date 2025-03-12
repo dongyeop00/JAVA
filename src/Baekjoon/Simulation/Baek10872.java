106817d03da09527a59f67f0b1c8f016ab11c8e9
dongyeop00 <ehd_duq94@naver.com>
2025-03-12 15:19:12 +0900

    25.03.12 : 팩토리얼

---
+++ src/Baekjoon/Simulation/Baek10872.java
@@ -0,0 +1,28 @@
+package Baekjoon.Simulation;
+
+import java.util.Scanner;
+
+public class Baek10872 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        if(N == 0){
+            System.out.println(1);
+            return;
+        }else{
+            System.out.println(fact(N));
+        }
+    }
+
+    private static int fact(int temp){
+        int num = 1;
+
+        for(int i=temp; i>0; i--){
+            num *= i;
+        }
+
+        return num;
+    }
+}
