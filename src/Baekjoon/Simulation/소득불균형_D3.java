528003fb3ee002e9630aa0b69c6d87ba4b4d7a8f
구동엽 <ehd_duq94@naver.com>
2025-04-18 10:10:03 +0900

    25.04.18 : D3 구현

---
+++ src/Baekjoon/Simulation/소득불균형_D3.java
@@ -0,0 +1,37 @@
+package Baekjoon.Simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 소득불균형_D3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for(int test_case = 1; test_case<=T; test_case++){
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int[] array = new int[N];
+            int sum = 0, count = 0;
+
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for(int i=0; i<N; i++){
+                int temp = Integer.parseInt(stringTokenizer.nextToken());
+                array[i] = temp;
+                sum += temp;
+            }
+
+            sum /= N;
+
+            for(int i=0; i<N; i++){
+                if(sum >= array[i]){
+                    count++;
+                }
+            }
+
+            System.out.println("#"+test_case+" "+count);
+        }
+    }
+}
---
+++ src/Baekjoon/Simulation/장애물경주난이도_D3.java
@@ -0,0 +1,40 @@
+package Baekjoon.Simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 장애물경주난이도_D3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        for(int test_case = 1; test_case<=T; test_case++){
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int[] array = new int[N];
+            int up = 0;
+            int down = 0;
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            for(int i=0; i<N; i++){
+                array[i] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            for(int i=0; i<N-1; i++){
+                if(array[i] < array[i+1]){
+                    int temp = array[i+1] - array[i];
+                    up = Math.max(temp,up);
+                }else{
+                    int temp = array[i] - array[i+1];
+                    down = Math.max(temp, down);
+                }
+
+            }
+
+            System.out.println("#"+test_case+" "+up+" "+down);
+        }
+    }
+}
