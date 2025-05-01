f2dc34b32d3e2a925c10a12462380a059a0fb86c
dongyeop00 <ehd_duq94@naver.com>
2025-05-01 23:49:53 +0900

    25.05.01: 일요일(인데스 사용)

---
+++ src/Baekjoon/Simulation/일요일_D3.java
@@ -0,0 +1,33 @@
+package Baekjoon.Simulation;
+
+import java.util.Scanner;
+
+public class 일요일_D3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        String[] weeks = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+        for(int test_case = 1; test_case<=T; test_case++){
+            int sun = 6;
+            int current = 0;
+            int result = 0;
+            String str = sc.next();
+
+            for(int i=0; i<7; i++){
+                if(str.equals(weeks[i])){
+                    current = i;
+                }
+            }
+
+            if(current == 6){
+                result = 7;
+            }else{
+                result = sun - current;
+            }
+
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
+}
