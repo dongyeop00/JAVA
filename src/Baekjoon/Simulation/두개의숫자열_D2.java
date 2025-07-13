aa2c86699b76bc48c6789dfb5dab5ba87157adf5
dongyeop00 <ehd_duq94@naver.com>
2025-07-13 22:16:01 +0900

    25.07.13 : D2 문제 풀이

---
+++ src/Baekjoon/Simulation/두개의숫자열_D2.java
@@ -0,0 +1,79 @@
+package Baekjoon.Simulation;
+
+import java.io.*;
+import java.util.*;
+
+public class  두개의숫자열_D2 {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for(int test_case = 1; test_case <= T; test_case++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            int N = Integer.parseInt(stringTokenizer.nextToken());
+            int M = Integer.parseInt(stringTokenizer.nextToken());
+
+            int[] A = new int[N];
+            int[] B = new int[M];
+
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for(int i=0; i<N; i++) {
+                A[i] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for(int i=0; i<M; i++) {
+                B[i] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            int result = 0;
+
+            if(N < M) {
+                result = method(A, B, M-N+1, N, 0);
+            }else if(N > M){
+                result = method(A, B, N-M+1, M, 1);
+            }else {
+                result = 0;
+            }
+
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
+
+    public static int method(int[] A, int[] B, int windowSize, int multSize, int mode) {
+        int startIndex = 0;
+        int si = 1;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int temp = 0;
+
+        if(mode == 0) {
+            for(int i=0; i<windowSize; i++) {
+                for(int j=0; j<multSize; j++) {
+                    sum += A[temp++] * B[startIndex++];
+                }
+
+                max = Math.max(sum, max);
+                temp = 0;
+                startIndex = si++;
+                sum = 0;
+            }
+        }else {
+            for(int i=0; i<windowSize; i++) {
+                for(int j=0; j<multSize; j++) {
+                    sum += B[temp++] * A[startIndex++];
+                }
+
+                max = Math.max(sum, max);
+                temp = 0;
+                startIndex = si++;
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/Baekjoon/Simulation/파리퇴치3_D2.java
@@ -0,0 +1,64 @@
+package Baekjoon.Simulation;
+
+import java.util.*;
+import java.io.*;
+
+public class 파리퇴치3_D2 {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            int N = Integer.parseInt(stringTokenizer.nextToken());
+            int M = Integer.parseInt(stringTokenizer.nextToken());
+
+            int[][] map = new int[N][N];
+            int max = Integer.MIN_VALUE;
+
+            for(int i=0; i<N; i++) {
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                for(int j=0; j<N; j++) {
+                    map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                }
+            }
+
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    int plusSum = map[i][j];
+                    int diaSum = map[i][j];
+
+                    for(int k=1; k<M; k++) {
+                        // + 모양
+                        //상
+                        if(i-k >= 0) plusSum += map[i-k][j];
+                        //우
+                        if(j+k < N) plusSum += map[i][j+k];
+                        //하
+                        if(i+k < N) plusSum += map[i+k][j];
+                        //좌
+                        if(j-k >= 0) plusSum += map[i][j-k];
+
+                        // X 모양
+                        // 좌상
+                        if(i-k >= 0 && j-k >= 0) diaSum += map[i-k][j-k];
+                        // 우상
+                        if(i-k >= 0 && j+k < N) diaSum += map[i-k][j+k];
+                        // 좌하
+                        if(i+k < N && j-k >= 0) diaSum += map[i+k][j-k];
+                        // 우하
+                        if(i+k < N && j+k < N) diaSum += map[i+k][j+k];
+                    }
+
+                    max = Math.max(max, Math.max(plusSum, diaSum));
+                }
+            }
+
+            System.out.println("#" + test_case + " " + max);
+        }
+    }
+}
